0bdc37b67c1baf366621ed4b8ec829be42b1d5d9
felipeam10 <felipeam10@hotmail.com>
2022-08-13 00:15:01 -0300

    v4.31 cenario: inserir conta com sucesso

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -4,9 +4,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        features = "src/test/resources/inserirConta.feature",
         glue = "br.mg.paodequeijo.steps",
+//        tags = "not @ignore",
         plugin = {"pretty", "html:target/cucumber.html"},
-        monochrome = true,
+        monochrome = false,
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         dryRun = true
     )
--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -4,11 +4,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        features = "src/test/resources/inserirConta.feature",
         glue = "br.mg.paodequeijo.steps",
+        tags = "not @ignore",
         plugin = {"pretty", "html:target/cucumber.html"},
         monochrome = false,
-        snippets = CucumberOptions.SnippetType.CAMELCASE,
-        dryRun = true
+        snippets = CucumberOptions.SnippetType.CAMELCASE
     )
 public class RunnerTest {
 
