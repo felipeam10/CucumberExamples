61250dc840036fafb26a085bb256c83aa3fe0435
felipeam10 <felipeam10@hotmail.com>
2022-08-17 00:47:13 -0300

    v4.35 gambiarra para massa de dados -  reseta tudo antes de começar os testes

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -1,6 +1,10 @@
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -14,4 +18,15 @@ import org.junit.runner.RunWith;
     )
 public class RunnerTest {
 
+    @BeforeClass
+    public static void reset(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\workspace-projects\\NavegDrivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://seubarriga.wcaquino.me/");
+        driver.findElement(By.id("email")).sendKeys("felipeam10@hotmail.com");
+        driver.findElement(By.id("senha")).sendKeys("123456");
+        driver.findElement(By.tagName("button")).click();
+        driver.findElement(By.linkText("reset")).click();
+        driver.quit();
+    }
 }
